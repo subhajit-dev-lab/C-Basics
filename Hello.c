02dfbe64985c0f1c52ecd4e5ab77cf86dd399600
subhajit-dev-lab <subhajitmahato3010@gmail.com>
2026-06-23 21:36:06 +0530

    Print Code

--- Hello.c
+++ Hello.c
@@ -3,11 +3,7 @@
 int main(){
     printf ("HelloWorld \n");
 
-<<<<<<< HEAD
     printf ("Thank You \n");
-=======
-    printf ("Thank You");
->>>>>>> bd1dfa40d24282d2bd08e9d594877f4f2ea82c1a
 
     return 0;
 }
