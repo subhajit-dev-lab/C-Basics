9cc041919acf21997ad4074614b0cf70caff9e47
subhajit-dev-lab <subhajitmahato3010@gmail.com>
2026-06-23 21:22:30 +0530

    Area Calculation Code

---
+++ Area_Calculation/Area_of_Circle.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+
+    int radius;
+
+    printf ("enter radius: ");
+    scanf ("%d" , &radius);
+
+    float pi;
+    pi = 3.14;
+
+    printf ("Area Of Circle: %f \n" ,pi*radius*radius);
+
+    return 0;
+}
---
+++ Area_Calculation/Area_of_Square.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(){
+
+    int side;
+
+    printf ("Enter Side: ");
+    scanf ("%d" , &side);
+
+    printf ("Area of Square : %d" , side*side); //Any Formula Can Be Coded
+
+    return 0;
+}
