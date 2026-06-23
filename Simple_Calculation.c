a2496bb6d1e77d6236022e9d3bfc2a11b6a1e9b7
subhajit-dev-lab <subhajitmahato3010@gmail.com>
2026-06-23 20:45:10 +0530

    Print & Calculation

---
+++ Simple_Calculation.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+// Simple Mathematics Calculation Code
+
+int main (){
+
+    int a,b ;
+
+    printf ("Enter a: \n");
+    scanf ("%d" ,&a);
+
+    printf ("Enter b: \n");
+    scanf ("%d" ,&b);
+    
+    printf ("Sum is: %d \n" ,a+b); //Any Calculation Can Happen (+,-,*,/)
+
+    return 0;
+}
