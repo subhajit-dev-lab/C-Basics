ec16b59c9a7cdb62b6f221a4e04b19ac14a0b865
subhajit-dev-lab <subhajitmahato3010@gmail.com>
2026-06-24 12:20:08 +0530

    Identification Of Even Or Odd

---
+++ Operators/Condition 1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <math.h>
+
+// Write A Program To Check If A Number Divisible By 2 Or Not
+
+int main (){
+
+    int x ;
+
+    printf ("Enter Value Of x : ");
+    scanf ("%d" , &x);
+
+    printf ("%d" , x % 2 == 0);
+
+    return 0;
+}
