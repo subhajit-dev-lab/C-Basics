d45065d14fdda0523c4fcd44bb5deef71264fab5
subhajit-dev-lab <subhajitmahato3010@gmail.com>
2026-06-24 13:07:01 +0530

    Perimeter Of Rectangle

---
+++ Practice_Set/Problem_1.c
@@ -0,0 +1,19 @@
+// Write A Program To Calculate Perimeter Of Rectangle
+
+#include <stdio.h>
+
+int main(){
+
+int a,b ;
+
+printf ("Enter a: ");
+scanf ("%d" , &a);
+
+printf ("Enter b: ");
+scanf ("%d" , &b);
+
+printf ("Perimeter of Rectangle: %d" , (2*(a+b)));
+
+return 0;
+
+}
