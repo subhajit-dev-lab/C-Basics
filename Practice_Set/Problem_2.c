acea8e4161e75832aef1d8f987998e8b867886c8
subhajit-dev-lab <subhajitmahato3010@gmail.com>
2026-06-24 13:24:53 +0530

    Cube calculation By Power

---
+++ Practice_Set/Problem_2.c
@@ -0,0 +1,17 @@
+// Cube Calculation of A Number By Power
+
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+
+    int n;
+
+    printf ("enter n: ");
+    scanf ("%d" , &n);
+
+    printf ("Answer Is: %d" , (int)pow(n,3)); // it means n to the power 3
+
+    return 0;
+
+}
