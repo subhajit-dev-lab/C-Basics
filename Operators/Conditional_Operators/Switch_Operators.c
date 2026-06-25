b0c2268efb2010a3971d19072835a1bc517aa452
subhajit-dev-lab <subhajitmahato3010@gmail.com>
2026-06-25 11:08:24 +0530

    Day Identification In A Week

---
+++ Operators/Conditional_Operators/Switch_Operators.c
@@ -0,0 +1,33 @@
+// Identification Of A Day In A Week
+
+#include <stdio.h>
+
+int main (){
+
+    int Day ;
+
+    printf ("Enter Day(1-7): ");
+    scanf ("%d" , &Day) ;
+
+    switch (Day) {
+        case 1 : printf ("Monday \n") ;
+                break;
+        case 2 : printf ("Tueaday \n");
+                break;
+        case 3 : printf ("Wednesday \n");
+                break;
+        case 4 : printf ("Thursday \n");
+                break;
+        case 5 : printf ("Friday \n") ;
+                break;
+        case 6 : printf ("Saturday \n");
+                break;
+        case 7 : printf ("Sunday \n");
+                break;
+
+        default : printf ("Not A Valid Day \n");
+
+    }
+
+    return 0;
+}
