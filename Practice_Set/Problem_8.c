366c5fe90daac85c131621a417f4811d30b151d9
subhajit-dev-lab <subhajitmahato3010@gmail.com>
2026-06-26 11:09:00 +0530

    Unsolved Problem

---
+++ Practice_Set/Problem_8.c
@@ -0,0 +1,10 @@
+// Write A Program To Check If A Given Number Is Armstrong Number Or Not
+
+//Armstrong Number : Number = (digit_1)^n + (digit_2)^n + ..... + (digit_n)^n ; [ n = number of digits ]  
+
+#include <stdio.h>
+
+int main (){
+
+    
+}
