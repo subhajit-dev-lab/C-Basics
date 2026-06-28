fe8c75d9fe7437af279c578fc4876fbd37c25b5d
subhajit-dev-lab <subhajitmahato3010@gmail.com>
2026-06-28 12:34:49 +0530

    Increment Operators

---
+++ Operators/Increment_Operator/++i_.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main () {
+
+    int i = 1 ;
+
+    printf ("%d \n" , ++i) ; // ++i Means Increase, Then Use
+    printf ("%d \n" , i ) ;
+
+    return 0 ;
+}
---
+++ Operators/Increment_Operator/i++_.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main () {
+
+    int i = 1 ;
+
+    printf ("%d \n" , i++) ; // i++ Means Use, Then Increase
+    printf ("%d \n" , i ) ;
+
+    return 0 ;
+}
