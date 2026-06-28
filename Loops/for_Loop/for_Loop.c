e84cfeae927dd178c94d7affae83e442de73f564
subhajit-dev-lab <subhajitmahato3010@gmail.com>
2026-06-28 11:44:06 +0530

    Printing Same Thing Multiple Times

---
+++ Loops/for_Loop/for_Loop.c
@@ -0,0 +1,12 @@
+// for Loop Structure : for (initialisation ; condition ; updation){........}
+
+#include <stdio.h>
+
+int main(){
+
+for (int i=1 ; i<=10 ; i=i+1){  // If We Need 1000 print then i<=1000
+
+    printf ("Hello World \n") ;
+}
+    return 0 ;
+}
