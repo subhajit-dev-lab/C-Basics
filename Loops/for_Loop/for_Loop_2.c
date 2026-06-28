ecde8a5a5d95bafbf44fb152b5a386c4ae51148e
subhajit-dev-lab <subhajitmahato3010@gmail.com>
2026-06-28 12:03:05 +0530

    Printing Of 1-100

--- Loops/for_Loop/for_Loop_2.c
+++ Loops/for_Loop/for_Loop_2.c
@@ -10,3 +10,16 @@ int main(){
     }
     return 0 ;
 }
+
+// if want to print 10 to 1 then :
+
+// #include <stdio.h>
+
+// int main(){
+
+//     for (int i = 10 ; i >= 1 ; i = i-1) {  // If Want To Print 1000-1 Then i=1000 
+
+//         printf("%d \n" , i) ;
+//     }
+//     return 0 ;
+// }
