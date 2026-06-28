670939cb63fe2db5a4cf3a029b9f06e1107bdad1
subhajit-dev-lab <subhajitmahato3010@gmail.com>
2026-06-28 11:58:33 +0530

    Printing Of 1-100

---
+++ Loops/for_Loop/for_Loop_2.c
@@ -0,0 +1,12 @@
+// for Loop Structure : for (initialisation ; condition ; updation){........}
+
+#include <stdio.h>
+
+int main(){
+
+    for (int i = 1 ; i <= 100 ; i = i+1) {  // If Want To Print 1-1000 Then i<=1000 
+
+        printf("%d \n" , i) ;
+    }
+    return 0 ;
+}
