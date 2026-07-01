7439d5a08817763e52d9bc35a7281dcecb890d36
subhajit-dev-lab <subhajitmahato3010@gmail.com>
2026-07-01 17:45:28 +0530

    Exit From A loop

---
+++ Loops/Break_Statement.c
@@ -0,0 +1,16 @@
+// Break is used when we have to exit from a loop
+
+#include <stdio.h>
+
+int main (){
+
+    for (int i=1 ; i<=5 ; i++) {
+        if (i == 3){
+            break ;
+        }
+        printf("%d\n" , i);
+    }
+        printf ("End");
+    return 0;
+    
+}
