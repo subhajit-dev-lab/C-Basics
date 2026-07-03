2773d434f16001847d4aba6ea9616a7010e343fe
subhajit-dev-lab <subhajitmahato3010@gmail.com>
2026-07-03 11:15:32 +0530

    Printing 1-10 except 6

---
+++ Practice_Set/Problem_16.c
@@ -0,0 +1,15 @@
+// Print all the numbers from 1-10 except for 6
+
+#include <stdio.h>
+
+int main (){
+
+    for (int i=1 ; i<=10 ; i++){
+
+        if (i==6){
+            continue;
+        }
+        printf("%d \n" , i);
+    }
+    return 0;
+}
