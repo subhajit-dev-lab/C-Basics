6fb3f09ecb53d946bfea5e56bd6b326a47a24c60
subhajit-dev-lab <subhajitmahato3010@gmail.com>
2026-07-08 16:18:07 +0530

    Function_Syntax

---
+++ Functions_&_Recurtion/Print_Hello.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+// Declaration / Prototype
+void printhello();
+
+// Function call
+int main (){
+    printhello();
+    printhello();
+    printhello();
+    printhello();
+    printhello();
+    return 0;
+}
+
+//Function Defination
+void printhello(){
+    printf ("HelloWorld\n");
+}
