c1e3a070a62a202dd4f003c149c526060e56d746
subhajit-dev-lab <subhajitmahato3010@gmail.com>
2026-07-09 23:14:11 +0530

    Table printing by function

--- Functions_&_Recurtion/Print_Table.c
+++ Functions_&_Recurtion/Print_Table.c
@@ -7,11 +7,11 @@ int main (){
     printf ("Enter Number : ");
     scanf ("%d", &n);
 
-    printTable(n) ; // argument / Actual parameter
+    printTable(n) ; // argument / Actual parameter (To send value)
 
     return 0;
 }
-void printTable(int n){  // Parameter / Formal parameter
+void printTable(int n){  // Parameter / Formal parameter (To receive value)
     for (int i=1 ; i<=10 ; i++){
         printf ("%d\n" ,i*n);
     }
