a67d83626a1a2ca1298d25c73ccb68971d0439b0
subhajit-dev-lab <subhajitmahato3010@gmail.com>
2026-07-15 15:23:21 +0530

    Celsius to Fahrenheit

---
+++ Practice_Set/Problem_25.c
@@ -0,0 +1,18 @@
+// Write a function to convert celsius to fahrenheit [F = (C * 9/5) + 32]
+
+#include <stdio.h>
+
+float calculateTemp(float celsius);
+
+int main (){
+
+    float far = calculateTemp(0); // Since we are converting 0 degree cesius to fahrenheit, if we want to convert 'n' degree then 0=n
+    printf ("Fahrenheit value is: %f" , far);
+    return 0;
+}
+
+float calculateTemp(float celsius){
+
+    float far = (celsius*9/5)+32 ;
+    return far;
+}
