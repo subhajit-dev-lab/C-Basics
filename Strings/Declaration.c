142551fba14075a8166e046045ddc7f08c9bd8ba
subhajit-dev-lab <subhajitmahato3010@gmail.com>
2026-08-09 21:48:38 +0530

    Declaration of Strings

---
+++ Strings/Declaration.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main (){
+    char name[] = {'S','U','B','H','A','J','I','T','\0'};     // '\0' is a null character
+    printf ("%s\n", name);
+    return 0;
+}
+
+// We can also write it by 
+//      char name[]={"SUBHAJIT"};
