f43409c6eee0d7f6c11b3c2d5c4b7993b30af922
subhajit-dev-lab <subhajitmahato3010@gmail.com>
2026-09-03 22:32:14 +0530

    Printing length of a name

--- Practice_Set/Problem_34.c
+++ Practice_Set/Problem_34.c
@@ -22,3 +22,16 @@ int count_Length (char arr[]){
     }
     return count-1;
 }
+
+// Optional Methode
+
+// #include <stdio.h>
+// #include <string.h>
+
+// int main (){
+//     char name[] = "subhajit";
+//     int length = strlen(name);
+//     printf ("Length is: %d", length);
+
+//     return 0;
+// }
